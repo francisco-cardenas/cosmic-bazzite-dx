6281099ad9fbc5eaa25dd031b9bd22417419ca74
cardenas-sec <github.com.coral702@passmail.com>
2025-11-14 00:01:33 -0700

    Corrected rawhide repo and inscluded ZSH install

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,11 +10,12 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # Install base tools
-dnf5 install -y tmux
+dnf5 install -y tmux zsh
 
 ### Install COSMIC Desktop Environment
-# Add the Fedora Rawhide repo temporarily to access COSMIC packages
-dnf5 -y addrepo --from-repofile=https://download.fedoraproject.org/pub/fedora/linux/development/rawhide/Everything/x86_64/os/
+# Install Rawhide repository definitions (kept disabled)
+# This does NOT activate the Rawhide repo globally.
+dnf5 install -y fedora-repos-rawhide
 
 # Install COSMIC and related components
 dnf5 install -y \
@@ -23,7 +24,8 @@ dnf5 install -y \
     cosmic-term \
     cosmic-files \
     cosmic-settings \
-    cosmic-app-library
+    cosmic-app-library \
+    --enablerepo=rawhide
 
 # Remove the temporary Rawhide repo after installation
 dnf5 -y removerepo rawhide

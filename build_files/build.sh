3f28a10a9cd43f75f047fff707534847740b26fb
cardenas-sec <github.com.coral702@passmail.com>
2025-11-13 23:36:19 -0700

    Cosmic install added to build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,20 +5,29 @@ set -ouex pipefail
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
+# RPMfusion repos are available by default in ublue main images.
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# Install base tools
+dnf5 install -y tmux
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+### Install COSMIC Desktop Environment
+# Add the Fedora Rawhide repo temporarily to access COSMIC packages
+dnf5 -y addrepo --from-repofile=https://download.fedoraproject.org/pub/fedora/linux/development/rawhide/Everything/x86_64/os/
+
+# Install COSMIC and related components
+dnf5 install -y \
+    cosmic-desktop-environment \
+    cosmic-session \
+    cosmic-term \
+    cosmic-files \
+    cosmic-settings \
+    cosmic-app-library
+
+# Remove the temporary Rawhide repo after installation
+dnf5 -y removerepo rawhide
 
 #### Example for enabling a System Unit File
-
 systemctl enable podman.socket
+

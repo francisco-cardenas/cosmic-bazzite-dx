7851c5d03e94b8f816e1a00a797a2aec13bc68b8
francisco-cardenas <github.twiddling048@passmail.com>
2025-11-14 00:25:45 -0700

    Cosmic install added to build.sh (#5)
    * Cosmic install added to build.sh
    
    * Corrected rawhide repo and inscluded ZSH install
    
    * Corrected cosmic install packages. Dropped rawhide repo
    
    ---------
    
    Co-authored-by: cardenas-sec

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,20 +5,44 @@ set -ouex pipefail
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
+# RPMfusion repos are available by default in ublue main images.
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# Install base tools
+dnf5 install -y tmux zsh
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+### Install COSMIC Desktop Environment
+
+# Install COSMIC and related components
+dnf5 install -y \
+    cosmic-edit \
+    cosmic-files \
+    cosmic-player \
+    cosmic-session \
+    cosmic-store \
+    cosmic-app-library \
+    cosmic-applets \
+    cosmic-bg \
+    cosmic-comp \
+    cosmic-config-fedora \
+    cosmic-greeter \
+    cosmic-icon-theme \
+    cosmic-idle \
+    cosmic-initial-setup \
+    cosmic-launcher \
+    cosmic-notifications \
+    cosmic-osd \
+    cosmic-panel \
+    cosmic-randr \
+    cosmic-screenshot \
+    cosmic-settings \
+    cosmic-settings-daemon \
+    cosmic-term \
+    cosmic-workspaces \
+    xdg-desktop-portal-cosmic \
+    cosmic-wallpapers
 
 #### Example for enabling a System Unit File
-
 systemctl enable podman.socket
+

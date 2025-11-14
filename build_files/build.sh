cb7c50b762387d595fa71407f937627db0925672
cardenas-sec <github.com.coral702@passmail.com>
2025-11-14 00:14:49 -0700

    Corrected cosmic install packages. Dropped rawhide repo

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,22 +13,28 @@ set -ouex pipefail
 dnf5 install -y tmux zsh
 
 ### Install COSMIC Desktop Environment
-# Install Rawhide repository definitions (kept disabled)
-# This does NOT activate the Rawhide repo globally.
-dnf5 install -y fedora-repos-rawhide
 
 # Install COSMIC and related components
 dnf5 install -y \
-    cosmic-desktop-environment \
+    cosmic-comp \
     cosmic-session \
-    cosmic-term \
-    cosmic-files \
     cosmic-settings \
-    cosmic-app-library \
-    --enablerepo=rawhide
-
-# Remove the temporary Rawhide repo after installation
-dnf5 -y removerepo rawhide
+    cosmic-launcher \
+    cosmic-files \
+    cosmic-edit \
+    cosmic-term \
+    cosmic-panel \
+    cosmic-applets \
+    cosmic-workspace \
+    cosmic-osd \
+    cosmic-notifications \
+    cosmic-screenshot \
+    cosmic-greeter \
+    cosmic-bg \
+    cosmic-icons \
+    cosmic-gtk-theme \
+    cosmic-wallpapers \
+    cosmic-applibrary
 
 #### Example for enabling a System Unit File
 systemctl enable podman.socket

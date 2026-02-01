80add7bc0cdf939feb88fc953b9aa563a99b4f85
Francisco Cardenas <github.twiddling048@passmail.com>
2026-01-31 20:52:57 -0700

    Added fido2-tools to dnf install

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # Install base tools
-dnf5 install -y tmux zsh cups hplip
+dnf5 install -y tmux zsh cups hplip fido2-tools
 
 
 # Install COSMIC and related components

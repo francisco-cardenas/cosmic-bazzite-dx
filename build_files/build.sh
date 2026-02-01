84a9a27784b16dbad7799e74715c936d0a859858
Francisco Cardenas <github.twiddling048@passmail.com>
2026-01-31 21:03:27 -0700

    Added fido2 support in dracut

--- build_files/build.sh
+++ build_files/build.sh
@@ -52,3 +52,6 @@ install -Dm644 /ctx/files/etc/skel/.zshrc /usr/share/defaults/skel/.zshrc
 
 ### Post install script to $HOME
 install -Dm755 /ctx/files/etc/skel/post-install.sh /usr/share/defaults/skel/.post-install.sh
+
+# Dracut snippet for initramfs to include fido2 support
+install -Dm0644 /ctx/files/etc/dracut.conf.d/fido2.conf /etc/dracut.conf.d/fido2.conf

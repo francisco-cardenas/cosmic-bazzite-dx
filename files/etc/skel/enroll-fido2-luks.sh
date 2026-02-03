e5049172da976026d30ef98d9dc6bd4853fd8d6f
Francisco Cardenas <github.twiddling048@passmail.com>
2026-02-03 14:32:16 -0700

    fix: Change logging to stderr

--- files/etc/skel/enroll-fido2-luks.sh
+++ files/etc/skel/enroll-fido2-luks.sh
@@ -15,9 +15,9 @@
 set -euo pipefail
 
 # Status print helpers
-info()  { echo -e "\e[1;34m[INFO]\e[0m $*"; }
-warn()  { echo -e "\e[1;33m[WARN]\e[0m $*"; }
-error() { echo -e "\e[1;31m[ERROR]\e[0m $*"; }
+info()  { echo -e "\e[1;34m[INFO]\e[0m $*" >&2; }
+warn()  { echo -e "\e[1;33m[WARN]\e[0m $*" >&2; }
+error() { echo -e "\e[1;31m[ERROR]\e[0m $*" >&2; }
 
 usage() {
   cat <<'EOF'
